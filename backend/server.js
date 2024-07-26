0cf07cc683259b8b8987684195464758b4a534d6
mahermubeen <mahermubeenahmad@gmail.com>
2024-07-26 20:48:05 +0500

    fix: change in server.js

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,19 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
+// if (ENV_VARS.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/public")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "public", "index.html"));
+//   });
+// }
+
 if (ENV_VARS.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/public")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
