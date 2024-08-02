6ebf272bb44864b1b74e6a3487ef3a6f3eefa599
mahermubeen <mahermubeenahmad@gmail.com>
2024-08-03 02:17:40 +0500

    fix: its final done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this matches the Vercel setting
+  },
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:3000",
       },
     },
+  },
+  optimizeDeps: {
+    include: ["regenerator-runtime/runtime"],
   },
 });
