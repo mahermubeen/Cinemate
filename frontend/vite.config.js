4603c3f254c735ad0b69e640fada3164a0d89323
mahermubeen <mahermubeenahmad@gmail.com>
2024-08-03 00:24:47 +0500

    fix: change in vite.config.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-
-  return {
-    plugins: [react()],
-    build: {
-      outDir: "../public",
-    },
-    server: {
-      proxy: isProduction
-        ? {} // No proxy in production, set API base URL directly in your code
-        : {
-            "/api": {
-              target: "http://localhost:3000",
-              changeOrigin: true,
-            },
-          },
-    },
-    define: {
-      "process.env": {
-        API_BASE_URL: isProduction
-          ? "https://cinemate-eta.vercel.app"
-          : "http://localhost:3000",
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
       },
     },
-    optimizeDeps: {
-      include: ["regenerator-runtime/runtime"],
-    },
-  };
+  },
+  optimizeDeps: {
+    include: ["regenerator-runtime/runtime"],
+  },
 });
