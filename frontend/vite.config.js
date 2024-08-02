0e5e4c5ee13e29e1f268b1617030332b915f1b70
mahermubeen <mahermubeenahmad@gmail.com>
2024-08-03 02:07:17 +0500

    fix: vite issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this matches the Vercel setting
+  },
   server: {
     proxy: {
       "/api": {
